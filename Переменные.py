b250a0f76ec2695a6adb282fa4bd3a6653fb8406
ValGorDMT <Valgordmt@gmail.com>
2024-10-24 05:52:31 +0300

    Create Переменные.py

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+task = 12
+time = 1.5
+name = 'Python'
+task_time = time / task
+print('Курс:', name, ', всего задач:', task, ', затрачено часов:', time, ', среднее время выполнения:', task_time, ' часа.')
